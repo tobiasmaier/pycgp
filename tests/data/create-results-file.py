5829ded3ce3133277d7d124db65b71134f957943
Tobias Maier <tobias.maier@unibas.ch>
2016-02-09 18:28:17 +0100

    PEP8 conformance

--- tests/data/create-results-file.py
+++ tests/data/create-results-file.py
@@ -14,31 +14,30 @@ def dataset_name(command, cell_order=None, label=None):
     if cell_order is not None:
         name = '%s/cell-order-%d' % (name, cell_order)
     if label is not None:
-        name = '%s/%d' %  (name, label)
+        name = '%s/%d' % (name, label)
     return name
-
 
 
 f = h5py.File(results_file, 'w')
 
 cgp_reader = cgp.GeometryReader(geometry_file, verbose=False)
 
-for cell_order in [0,1,2,3]:
+for cell_order in [0, 1, 2, 3]:
     f.create_dataset(dataset_name('max-label', cell_order),
                      data=cgp_reader.maxLabel(cell_order))
-for cell_order in [0,1,2,3]:
+for cell_order in [0, 1, 2, 3]:
     for label in range(1, cgp_reader.maxLabel(cell_order)+1):
         f.create_dataset(dataset_name('size', cell_order, label),
                          data=cgp_reader.size(cell_order, label))
-for cell_order in [0,1,2]:
+for cell_order in [0, 1, 2]:
     for label in range(1, cgp_reader.maxLabel(cell_order)+1):
         f.create_dataset(dataset_name('bounds', cell_order, label),
                          data=cgp_reader.bounds(cell_order, label))
-for cell_order in [1,2,3]:
+for cell_order in [1, 2, 3]:
     for label in range(1, cgp_reader.maxLabel(cell_order)+1):
         f.create_dataset(dataset_name('bounded-by', cell_order, label),
                          data=cgp_reader.boundedBy(cell_order, label))
-for cell_order in [1,2,3]:
+for cell_order in [1, 2, 3]:
     for label in range(1, cgp_reader.maxLabel(cell_order)+1):
         f.create_dataset(dataset_name('adjacent', cell_order, label),
                          data=cgp_reader.adjacent(cell_order, label))
@@ -57,12 +56,11 @@ for label in range(1, cgp_reader.maxLabel(3)+1):
                      data=cgp_reader.threeSet(label),
                      compression='lzf')
 
-for cell_order in [1,2,3]:
+for cell_order in [1, 2, 3]:
     for label in range(1, cgp_reader.maxLabel(cell_order)+1):
-        f.create_dataset(dataset_name('topological-point-set', cell_order, label),
-                         data=cgp_reader.topologicalPointSet(cell_order, label),
-                         compression='lzf')
+        f.create_dataset(
+            dataset_name('topological-point-set', cell_order, label),
+            data=cgp_reader.topologicalPointSet(cell_order, label),
+            compression='lzf')
 
 f.close()
-
-
