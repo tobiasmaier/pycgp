aa5a72c05941b9a7ad80a5d61bfee21212fe4d74
Tobias Maier <tobias.maier@unibas.ch>
2016-06-02 15:08:05 +0200

    use coordinate_type within CartesianPointSet

--- pycgp/cartesian_point_set.py
+++ pycgp/cartesian_point_set.py
@@ -1,8 +1,9 @@
 import numpy as np
 
+from .cgp_config import coordinate_type
 
 class CartesianPointSet():
     def __init__(self, topological_points, shape=None):
-        topological_points = np.array(topological_points, np.int)
+        topological_points = np.array(topological_points, coordinate_type)
         self.cartesian_points = np.concatenate(
-            (topological_points/2, (topological_points+1)/2), 0).astype(np.int)
+            (topological_points/2, (topological_points+1)/2), 0)
