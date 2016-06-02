66ff184be6d640dfa95a97b6c8bf18e4344065cc
Tobias Maier <tobias.maier@unibas.ch>
2016-06-02 15:17:49 +0200

    implemented extend and adjacent_voxels in TopologicalPointSet

--- pycgp/topological_point_set.py
+++ pycgp/topological_point_set.py
@@ -5,13 +5,25 @@ from .cgp_config import coordinate_type
 
 
 class TopologicalPointSet():
-    def __init__(self, cell_order, points):
+    def __init__(self, cell_order, points=np.zeros((0,3))):
         self.cell_order = cell_order
         self.points = np.abs(points)
         self.sign = 1 - 2 * np.any(np.asarray(points) < 0, axis=1).astype(np.int8)
 
+    def extend(self, tps):
+        """
+        Merges another TopologicalPointSet into this TopologicalPointSet
+        :param tps: another TopologicalPointSet
+        """
+        assert(self.cell_order == tps.cell_order)
+        self.points = np.vstack((self.points, tps.points))
+        self.sign = np.hstack((self.sign, tps.sign))
+
     def adjacent_voxels(self):
-        return np.array(map(self.point_to_voxel, self.points))
+        arr = np.zeros((2*len(self.points),3), dtype=coordinate_type)
+        arr[::2,:] = self.points / 2
+        arr[1::2,:] = (self.points + 1) / 2
+        return arr
 
     def size(self):
         return len(self.points)
